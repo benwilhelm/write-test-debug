9a610c4b34838b62deb4d049fc13a4b4beadb361
Ben Wilhelm <ben@screencastify.com>
2021-02-04 10:21:21 -0600

    flat html comp

---
+++ client/src/components/Board.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import './Board.css'
+
+const Board = () => {
+  return (
+    <div className="board">
+      <h2 className="board-title">Board Title</h2>
+      <div className="lists">
+        <div className="list">
+          <h3 className="list-title">List Title</h3>
+          <div className="cards">
+            <div className="card">
+              <h4 className="card-title">Card Title</h4>
+              <div className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eaque quisquam ex obcaecati nulla! Accusantium quod dolorum commodi deleniti, laborum. Illum accusantium minima, eligendi, obcaecati facere optio numquam voluptatibus repudiandae!</div>
+            </div>
+            <div className="card">
+              <h4 className="card-title">Card Title</h4>
+              <div className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eaque quisquam ex obcaecati nulla! Accusantium quod dolorum commodi deleniti, laborum. Illum accusantium minima, eligendi, obcaecati facere optio numquam voluptatibus repudiandae!</div>
+            </div>
+            <div className="card">
+              <h4 className="card-title">Card Title</h4>
+              <div className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eaque quisquam ex obcaecati nulla! Accusantium quod dolorum commodi deleniti, laborum. Illum accusantium minima, eligendi, obcaecati facere optio numquam voluptatibus repudiandae!</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="list">
+          <h3 className="list-title">List Title</h3>
+          <div className="cards">
+            <div className="card">
+              <h4 className="card-title">Card Title</h4>
+              <div className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eaque quisquam ex obcaecati nulla! Accusantium quod dolorum commodi deleniti, laborum. Illum accusantium minima, eligendi, obcaecati facere optio numquam voluptatibus repudiandae!</div>
+            </div>
+            <div className="card">
+              <h4 className="card-title">Card Title</h4>
+              <div className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eaque quisquam ex obcaecati nulla! Accusantium quod dolorum commodi deleniti, laborum. Illum accusantium minima, eligendi, obcaecati facere optio numquam voluptatibus repudiandae!</div>
+            </div>
+            <div className="card">
+              <h4 className="card-title">Card Title</h4>
+              <div className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eaque quisquam ex obcaecati nulla! Accusantium quod dolorum commodi deleniti, laborum. Illum accusantium minima, eligendi, obcaecati facere optio numquam voluptatibus repudiandae!</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="list">
+          <h3 className="list-title">List Title</h3>
+          <div className="cards">
+            <div className="card">
+              <h4 className="card-title">Card Title</h4>
+              <div className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eaque quisquam ex obcaecati nulla! Accusantium quod dolorum commodi deleniti, laborum. Illum accusantium minima, eligendi, obcaecati facere optio numquam voluptatibus repudiandae!</div>
+            </div>
+            <div className="card">
+              <h4 className="card-title">Card Title</h4>
+              <div className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eaque quisquam ex obcaecati nulla! Accusantium quod dolorum commodi deleniti, laborum. Illum accusantium minima, eligendi, obcaecati facere optio numquam voluptatibus repudiandae!</div>
+            </div>
+            <div className="card">
+              <h4 className="card-title">Card Title</h4>
+              <div className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eaque quisquam ex obcaecati nulla! Accusantium quod dolorum commodi deleniti, laborum. Illum accusantium minima, eligendi, obcaecati facere optio numquam voluptatibus repudiandae!</div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Board
